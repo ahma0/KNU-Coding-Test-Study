968c947c1915273f81415884405305843f8bf0da
KwonMinwoo <34360434+Kwonminwoo@users.noreply.github.com>
2023-05-21 14:40:44 +0900

    test 파일 삭제
    테스트용으로 만든 Main.java 파일 삭제

--- Kwonminwoo/Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
